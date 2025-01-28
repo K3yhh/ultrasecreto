c66fe374de61a5a5a6ceecc043203ec4b504fe91
K3yhh <lelygamer309@gmail.com>
2025-01-28 14:42:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const yesButton = document.querySelector(".btn--yes");
 const noButton = document.querySelector(".btn--no");
 const catImg = document.querySelector(".cat-img");
 
-const MAX_IMAGES = 6;
+const MAX_IMAGES = 10;
 
 let play = true;
 let noCount = 0;
@@ -48,6 +48,7 @@ function generateMessage(noCount) {
     "Es enserio Nath :(?",
     "Se me va a romper el corazoncito :'v",
     "Voy a llorar Nath...",
+    ">:'v",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
